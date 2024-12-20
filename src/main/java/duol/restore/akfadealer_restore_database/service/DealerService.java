0b83e93644dda0eca06484ed8ec329ef8a2b35c3
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-12-20 17:10:06 +0500

    csv copy

--- src/main/java/duol/restore/akfadealer_restore_database/service/DealerService.java
+++ src/main/java/duol/restore/akfadealer_restore_database/service/DealerService.java
@@ -16,13 +16,55 @@ import java.util.*;
 public class DealerService {
     private static Map<String, String> tableMap = new HashMap<>();
     static {
-        tableMap.put("dealer_role", "t_dealer_role");
-        tableMap.put("dealer_user", "t_dealer_user");
-        tableMap.put("dealer_client", "t_dealer_client");
-        tableMap.put("payment_receipt", "t_payment_receipt");
-        tableMap.put("invoice", "t_invoice");
-        tableMap.put("invoice_item", "t_invoice_item");
-        tableMap.put("log_history", "t_log_history");
+        tableMap.put("dealer_role", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " name, info, access_to_change_data, access_to_change_data_with_day, permissions, server_id, TRUE AS synced \n" +
+                " FROM \n" +
+                " akfadealer_web.t_dealer_role\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
+        tableMap.put("dealer_user", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " reg_user_cid AS created_user_id, edit_user_cid AS updated_user_id, name, login, pass, info, server_id, TRUE AS synced\n" +
+                " FROM \n" +
+                " akfadealer_web.t_dealer_user\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
+        tableMap.put("dealer_client", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " reg_user_cid AS created_user_id, edit_user_cid AS updated_user_id, printable_name, first_name, last_name, patronymic, passport_series, passport_number, birthday, phone, info, region_id AS region_sid, series AS series_sids, opening_balance, account_type, type, discount_card, currency_id AS currency_sid, checked_type, company_name, branch, server_id, TRUE AS synced\n" +
+                " FROM \n" +
+                " akfadealer_web.t_dealer_client\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
+        tableMap.put("payment_receipt", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " reg_user_cid AS created_user_id, edit_user_cid AS updated_user_id, date, from_client_cid AS from_client_id, to_client_cid AS to_client_id, type, amount, action_type, info, currency_id AS currency_sid, bind_cid AS bind_id, last_bind, server_id, TRUE AS synced\n" +
+                " FROM \n" +
+                " akfadealer_web.t_payment_receipt\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
+        tableMap.put("invoice", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " reg_user_cid AS created_user_id, edit_user_cid AS updated_user_id, date, type, client_cid AS client_id, discount_card, organization_type, server_id, TRUE AS synced\n" +
+                " FROM \n" +
+                " akfadealer_web.t_invoice\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
+        tableMap.put("invoice_item", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " reg_user_cid AS created_user_id, edit_user_cid AS updated_user_id, invoice_cid AS invoice_id, product_id AS product_sid, qty, alt_qty, actual_qty, rate, amount, income, action_type, currency_id AS currency_sid, bind_cid AS bind_id, last_bind, discount_percent, discount_amount, price_discount, price_discount_amount, server_id, TRUE AS synced\n" +
+                " FROM \n" +
+                " akfadealer_web.t_invoice_item\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
+        tableMap.put("log_history", "(SELECT \n" +
+                " cid AS id, reg_date AS created, update_date AS updated, status, \n" +
+                " date, terminal_id AS terminal_sid, class_name, exception_msg, server_id, TRUE AS synced\n" +
+                " FROM \n" +
+                " akfadealer_web.t_log_history\n" +
+                " WHERE\n" +
+                " dealer_id=%d)");
     }
 
     public List<Dealer> getDealers() {
@@ -68,8 +110,9 @@ public class DealerService {
 
     public void exportToCSV(String path, Long dealerId) throws SQLException, IOException {
         for (Map.Entry<String, String> entry: tableMap.entrySet()) {
+            System.out.println(entry.getKey().toUpperCase(Locale.ROOT) + " CSV export STARTING .....");
             File file = new File(path + "/" + entry.getKey() + "_" + dealerId + "_" + LocalDate.now() + ".csv");
-            CopyUtil.copyToFile(DatabaseConfig.getDatabaseConnection(), file.getAbsolutePath(), "(select * from akfadealer_web."+entry.getValue()+" where dealer_id=101)");
+            CopyUtil.copyToFile(DatabaseConfig.getDatabaseConnection(), file.getAbsolutePath(), String.format(entry.getValue(), dealerId));
             System.out.println(entry.getKey().toUpperCase(Locale.ROOT) + " CSV export completed successfully.");
         }
     }
